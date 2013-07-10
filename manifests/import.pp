83ba206d9ae17ca681adad0e2ae707a35643f288
Mark Kusch <mark.kusch@gmail.com>
2013-07-10 14:27:33 +0200

    docker images are now in gzip format

--- manifests/import.pp
+++ manifests/import.pp
@@ -10,17 +10,19 @@ define docker::import(
   validate_re($con_id, '^[\S]*$')
   validate_string($con_app, $con_user, $con_stage, $idx_srv, $idx_path)
 
+  $con_img_ext = 'lxc.tar.gz'
+
   $img_cache = '/var/cache/lxc/docker'
-  $con_uri = "${idx_srv}/${idx_path}/${con_user}/${con_app}/${con_id}.lxc.tar"
+  $con_uri = "${idx_srv}/${idx_path}/${con_user}/${con_app}/${con_id}.${con_img_ext}"
 
   exec{"fetch ${con_user}/${con_app}:${con_stage}":
-    command => "curl -q -s -k -L -C - --create-dirs -o ${img_cache}/${con_id}.lxc.tar ${con_uri}",
+    command => "curl -q -s -k -L -C - --create-dirs -o ${img_cache}/${con_id}.${con_img_ext} ${con_uri}",
     timeout => '240',
-    unless  => "test -f ${img_cache}/${con_id}.lxc.tar",
+    unless  => "test -f ${img_cache}/${con_id}.${con_img_ext}",
   }
 
   exec{"docker import ${con_user}/${con_app}:${con_stage}":
-    command => "docker import - ${con_user}/${con_app} ${con_stage} < ${img_cache}/${con_id}.lxc.tar",
+    command => "gzip -cd ${img_cache}/${con_id}.${con_img_ext} | docker import - ${con_user}/${con_app} ${con_stage}",
     timeout => '480',
     unless  => "docker images | grep ^${con_user}/${con_app}",
     require => Exec["fetch ${con_user}/${con_app}:${con_stage}"],
